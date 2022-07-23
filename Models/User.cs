a5cde1b56bf473d3259205648f46cc0ee424fdaa
MrLijan <liramj21@gmail.com>
2022-07-23 16:42:29 +0300

    Formatting

--- Models/User.cs
+++ Models/User.cs
@@ -2,13 +2,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace OnqueteApi.Models;
 
-[Index(nameof(Uuid),IsUnique = true)]
+[Index(nameof(Uuid), IsUnique = true)]
 [Index(nameof(Email), IsUnique = true)]
 public class User : BaseModel
 {
-  public int Id { get; set; }
-  public Guid Uuid { get; set; }
-  public string FirstName { get; set; }
-  public string LastName { get; set; }
-  public string Email { get; set; }
+    public int Id { get; set; }
+    public Guid Uuid { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
 }
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -8,46 +8,46 @@ namespace OnqueteApi.Services;
 
 public class UserService
 {
-  // Const
-  private readonly DataContext ctx;
-  private readonly ILogger<UserService> _logger;
+    // Const
+    private readonly DataContext ctx;
+    private readonly ILogger<UserService> _logger;
 
-  // Constructor
-  public UserService(DataContext dataContext, ILogger<UserService> logger)
-  {
-    ctx = dataContext;
-    _logger = logger;
-  }
-
-  //Methods
-  public async Task<List<User>> GetAll()
-  {
-    return await ctx.users.ToListAsync();
-  }
-
-  public async Task<ActionResult<User?>> GetByUuid(Guid uuid)
-  {
-    return await ctx.users.FirstOrDefaultAsync(u => u.Uuid == uuid);
-  }
-
-  public async Task<ActionResult<User?>> Add(CreateUserRequest user)
-  {
-    User newUser = new User()
+    // Constructor
+    public UserService(DataContext dataContext, ILogger<UserService> logger)
     {
-      Uuid = this.newGuid(),
-      FirstName = user.FirstName,
-      LastName = user.LastName,
-      Email = user.Email
-    };
+        ctx = dataContext;
+        _logger = logger;
+    }
 
-    var action = ctx.users.Add(newUser);
-    await ctx.SaveChangesAsync();
+    //Methods
+    public async Task<List<User>> GetAll()
+    {
+        return await ctx.users.ToListAsync();
+    }
 
-    return await ctx.users.FirstOrDefaultAsync(u => u.Uuid == newUser.Uuid);
-  }
+    public async Task<ActionResult<User?>> GetByUuid(Guid uuid)
+    {
+        return await ctx.users.FirstOrDefaultAsync(u => u.Uuid == uuid);
+    }
 
-  private Guid newGuid()
-  {
-    return Guid.NewGuid();
-  }
+    public async Task<ActionResult<User?>> Add(CreateUserRequest user)
+    {
+        User newUser = new User()
+        {
+            Uuid = this.newGuid(),
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            Email = user.Email
+        };
+
+        var action = ctx.users.Add(newUser);
+        await ctx.SaveChangesAsync();
+
+        return await ctx.users.FirstOrDefaultAsync(u => u.Uuid == newUser.Uuid);
+    }
+
+    private Guid newGuid()
+    {
+        return Guid.NewGuid();
+    }
 }
